c279ba6f4aafa429c3377285d8569ce6f1ff2889
Ian Jones <ianj1993@gmail.com>
2024-05-07 22:06:46 -0600

    Added logic for symbol selection modal

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let modal = document.getElementById("myModal");
 let span = document.getElementsByClassName("close")[0];
 let restartButton = document.getElementsByClassName("restart-button")[0];
+let symbolModal = document.getElementById("symbolModal");
+let computerButton = document.getElementById("computerButton");
+let playerButton = document.getElementById("secondPlayer");
+
+
 
 window.onload = function() {
     modal.style.display = "block";
@@ -13,3 +18,15 @@ span.onclick = function() {
 restartButton.onclick = function() {
     modal.style.display = "block";
 }
+
+playerButton.onclick = function() {
+    symbolModal.style.display = "block";
+}
+
+computerButton.onclick = function() {
+    symbolModal.style.display = "block";
+}
+
+
+
+
