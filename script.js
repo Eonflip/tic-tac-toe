27501b5f5f6429c1c2cf1a9b39aba8cedf4064f6
Ian Jones <ianj1993@gmail.com>
2024-05-14 16:35:14 -0600

    Added logic for switching between X, and O

--- script.js
+++ script.js
@@ -5,13 +5,48 @@ let restartButton = document.getElementsByClassName("restart-button")[0];
 let symbolModal = document.getElementById("symbolModal");
 let computerButton = document.getElementById("computerButton");
 let playerButton = document.getElementById("secondPlayer");
-const cellElements = document.querySelectorAll('[data-cell]')
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('gameBoard')
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+let circleTurn;
 
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, { once: true })
 })
 
+
+function handleClick(e) {
+    const tile = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(tile, currentClass);
+    // placeMark
+    // Check for Win
+    // Check for Draw
+    // Switch Turns
+    swapTurns();
+    setBoardHoverClass();
+}
+
+function placeMark(tile, currentClass) {
+    tile.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS);
+    } 
+    else {
+        board.classList.add(X_CLASS);
+    }
+}
 
 window.onload = function() {
     modal.style.display = "block";
